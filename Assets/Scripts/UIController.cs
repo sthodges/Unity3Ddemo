646b271b66b6cc3c41eeeeb1b47d727ba56f9ee5
Steve J. Hodges <sthodges@cabrillo.edu>
2017-03-01 16:35:50 -0800

    added dialog popup

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	[SerializeField] private Text scoreLabel;
+
+	[SerializeField] private SettingsPopup settingsPopup;
+
+	// Use this for initialization
+	void Start () {
+		settingsPopup.Close ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreLabel.text = Time.realtimeSinceStartup.ToString ();	
+	}
+
+
+	public void OnOpenSettings(){
+		//Debug.Log ("open settings");
+		settingsPopup.Open();
+	}
+
+}
