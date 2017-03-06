d2a7391187b7c259deb8f6836b4659c6d9abeb94
Steve J. Hodges <sthodges@cabrillo.edu>
2017-03-05 18:12:34 -0800

    temp remove inner and outer walls; replaced with code generation of outer walls

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour {
 	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] private GameObject wallPrefab;
 	private GameObject _enemy;
 
 	private string _mazeText;
 
 	// Use this for initialization
 	void Start () {
+		GameObject wall;
 		_mazeText = System.IO.File.ReadAllText ("Assets/mazesm.txt");
 		//Debug.Log (maze);
 		//Debug.Log (maze.Length);
+		// add some walls from the prefab programatically
+		// test code initial version:
+		//for (int i = 0; i < 10; i++) {
+		//	wall = Instantiate (wallPrefab) as GameObject;
+		//	wall.transform.position = new Vector3 (i*5, 5.0f, i*5);
+		//}
+		// create outer wall
+		for (int i = 0; i < 10; i++) {
+			wall = Instantiate (wallPrefab) as GameObject;
+			wall.transform.position = new Vector3 (i*5.0f-25.0f, 5.0f, -25.0f);
+			wall.transform.Rotate (0.0f, 90.0f, 0.0f);
+
+			wall = Instantiate (wallPrefab) as GameObject;
+			wall.transform.position = new Vector3 (i*5.0f-25.0f, 5.0f, 25.0f);
+			wall.transform.Rotate (0.0f, 90.0f, 0.0f);
+
+			wall = Instantiate (wallPrefab) as GameObject;
+			wall.transform.position = new Vector3 (-25.0f, 5.0f, i*5.0f-25.0f);
+
+			wall = Instantiate (wallPrefab) as GameObject;
+			wall.transform.position = new Vector3 (25.0f, 5.0f, i*5.0f-25.0f);
+	
+		}
+
+
+		// hardcoded to 5x5 maze encoded into length 25 maze string
+
+
+
 	}
 	
 	// Update is called once per frame
@@ -23,5 +54,17 @@ public class SceneController : MonoBehaviour {
 		float angle = Random.Range (0.0f, 360.0f);
 		_enemy.transform.Rotate (0.0f, angle, 0.0f);
 	}
-}
+
+
+
+	} // update
+
+	private bool hasRightWall(int wValue){
+		return (wValue & 1) > 0;
+	}
+
+	private bool hasDownWall(int wValue){
+		return (wValue & 2) > 0;
+	}
+
 }
