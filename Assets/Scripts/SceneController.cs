f70ec9b8c2a41613bc0544a768a5d4579e15fd81
Steve J. Hodges <sthodges@cabrillo.edu>
2017-04-13 17:28:59 -0700

    WIP: drawing walls; added DrawWallAt helper function and re-factored drawing outerwalls to use new code; next step: redrawing of interior walls

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,6 +9,11 @@ public class SceneController : MonoBehaviour {
 
 	private string _mazeText;
 
+	// note: wall prefabs are 1x by 10z
+	// so to figure out center point for drawing wall, depending on position adjust by
+	private float [] XWallOffset = { -5f, 0f, 5f, 0f };
+	private float[] ZWallOffset = { 0f, 5f, 0f, -5f };
+
 	// Use this for initialization
 	void Start () {
 		GameObject wall;
@@ -22,22 +27,21 @@ public class SceneController : MonoBehaviour {
 		//	wall.transform.position = new Vector3 (i*5, 5.0f, i*5);
 		//}
 		// create outer wall
-		for (int i = 0; i < 10; i++) {
-			/*
-			wall = Instantiate (wallPrefab) as GameObject;
-			wall.transform.position = new Vector3 (i*5.0f-25.0f, 2.5f, -25.0f);
-			wall.transform.Rotate (0.0f, 90.0f, 0.0f);
+		for (int i = 0; i < 5; i++) {
+			
+			//okay
+			drawWallAt (new Vector3 (i * 10.0f - 19.5f, 3.0f, -19.5f), 3); // top walls
 
-			wall = Instantiate (wallPrefab) as GameObject;
-			wall.transform.position = new Vector3 (i*5.0f-25.0f, 2.5f, 25.0f);
-			wall.transform.Rotate (0.0f, 90.0f, 0.0f);
+			// okay
+			drawWallAt (new Vector3 (i * 10.0f - 19.5f, 3.0f, 20.5f), 1); // bottom walls
 
-			wall = Instantiate (wallPrefab) as GameObject;
-			wall.transform.position = new Vector3 (-25.0f, 2.5f, i*5.0f-25.0f);
+			//okay
+			drawWallAt (new Vector3 (20.5f, 3.0f, i * 10.0f - 19.5f), 2); // left walls
 
-			wall = Instantiate (wallPrefab) as GameObject;
-			wall.transform.position = new Vector3 (25.0f, 2.5f, i*5.0f-25.0f);
-			*/
+			//okay
+			drawWallAt (new Vector3 (-29.5f, 3.0f, i * 10.0f - 19.5f), 2); // right walls
+
+
 		}
 
 
@@ -53,13 +57,13 @@ public class SceneController : MonoBehaviour {
 				temp = int.Parse (square, System.Globalization.NumberStyles.HexNumber);
 				//Debug.Log (square + " is " + temp);
 				if (hasRightWall(temp)){
-					wall = Instantiate (wallPrefab) as GameObject;
-					wall.transform.position = new Vector3 (i*10.0f-25.0f, 2.5f, j*10.0f-25.0f);
-					wall.transform.Rotate (0.0f, 90.0f, 0.0f);
+					//wall = Instantiate (wallPrefab) as GameObject;
+					//wall.transform.position = new Vector3 (i*10.0f-25.0f, 2.5f, j*10.0f-25.0f);
+					//wall.transform.Rotate (0.0f, 90.0f, 0.0f);
 				}
 				if (hasDownWall(temp)){
-					wall = Instantiate (wallPrefab) as GameObject;
-					wall.transform.position = new Vector3 (i*10.0f-25.0f, 2.5f, j*10.0f-25.0f);
+					//wall = Instantiate (wallPrefab) as GameObject;
+					//wall.transform.position = new Vector3 (i*10.0f-25.0f, 2.5f, j*10.0f-25.0f);
 					//wall.transform.Rotate (0.0f, 90.0f, 0.0f);
 				}
 			}
@@ -87,6 +91,18 @@ public class SceneController : MonoBehaviour {
 
 	private bool hasDownWall(int wValue){
 		return (wValue & 2) > 0;
+	}
+
+	// direction 0 Right, 1 Down, 2 Left, 3 up
+	private void drawWallAt(Vector3 position, int direction){
+		GameObject wall;
+		float thickness = 0.5f; //adjust coordinates for the thickness of the wall
+		Vector3 adjustedPosition = new Vector3 (position.x + XWallOffset [direction]-thickness, position.y, position.z + ZWallOffset [direction]-thickness);
+		wall = Instantiate (wallPrefab) as GameObject;
+		wall.transform.position = adjustedPosition;
+		if (direction == 1 || direction == 3)
+			wall.transform.Rotate (0.0f, 90.0f, 0.0f);
+
 	}
 
 }
